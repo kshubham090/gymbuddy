d179d9836c559399217ae8b7e03b3eca1dd1ac2f
Shubham Gupta <kshubham04907@gmail.com>
2025-01-03 01:52:04 +0530

    Add @vercel/analytics package and integrate analytics component in App

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import WorkoutDay from "./pages/WorkoutDay";
+import { Analytics } from "@vercel/analytics/react"
 
 const queryClient = new QueryClient();
 
@@ -17,10 +18,12 @@ const App = () => (
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/workout/:day" element={<WorkoutDay />} />
+          <Analytics />
         </Routes>
       </BrowserRouter>
     </TooltipProvider>
   </QueryClientProvider>
+  
 );
 
 export default App;
